8f000fc2332aee7e09042b33b1125df6c66df9b2
yurikayel <kayel.yuri@gmail.com>
2023-10-30 19:51:18 -0300

    @HiltAndroidApp @HiltViewModel

--- app/src/main/java/kayel/app/katz/KatzApp.kt
+++ app/src/main/java/kayel/app/katz/KatzApp.kt
@@ -3,6 +3,8 @@ package kayel.app.katz
 import android.app.Application
 import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kayel.app.katz.core.ComposableActivity
 import kayel.app.katz.ui.KatzNavigation
 import kayel.app.katz.ui.theme.KatzTheme
@@ -11,6 +13,7 @@ import kayel.app.katz.ui.viewmodel.KatzViewModel
 /**
  * KatzApp is the main application class that extends Application.
  */
+@HiltAndroidApp
 class KatzApp : Application()
 
 
@@ -18,10 +21,13 @@ class KatzApp : Application()
  * KatzActivity is an activity class that inherits from ComposableActivity
  * and manages the UI logic for the Katz application.
  */
+@HiltViewModel
 class KatzActivity : ComposableActivity()
 {
+	private val katzViewModel: KatzViewModel by viewModels()
+	
 	override var content: @Composable () -> Unit = {
-		KatzTheme { KatzNavigation(viewModels<KatzViewModel>().value) }
+		KatzTheme { KatzNavigation(katzViewModel) }
 	}
 }
 
